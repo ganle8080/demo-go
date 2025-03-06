aa71664fc4b8bc1b039afc261d03af769a2f015f
subingjie <kf.subingjie@cloudnineinfo.com>
2025-03-06 18:35:53 +0800

    add

--- src/user/controller.go
+++ src/user/controller.go
@@ -1,1 +1,12 @@
 package user
+
+type UserController struct {
+}
+
+func NewUserController() *UserController {
+	return &UserController{}
+}
+
+func (uc *UserController) GetUser(ctx *gin.Context) {
+	// do something
+}
